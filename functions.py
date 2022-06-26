d72744306093c1d09bf0dd077cbe6188081fbaab
João Carlos Pires <joaocarlosmrp@gmail.com>
2022-06-27 00:12:56 +0100

    Platform selection working; Compressing until the desired final size; Refactoring of code and build of structure to future code

--- functions.py
+++ functions.py
@@ -1,24 +1,26 @@
 import os
 from hurry.filesize import size
+from PIL import Image
 
 '''
 This function should receive a number from the frontend (once the user clicks on the platform icon)
 and should proceed with the corresponding compression rate / quality
 '''
-def get_platform(user_selection):
+def get_platform(user_selection, image, file_name):
     match user_selection:
-        case 1:
-            return "Facebook"
-        case 2:
-            return "Instagram"
-        case 3:
-            return "Twitter"
-        case 4:
-            return "LinkedIn"
-        case 5:
-            return "Discord"
-        case 6:
-            return "Messenger"
+        case 1: #Facebook
+            return compress_image(image, file_name, 8000000)
+        case 2: #Instagram
+            return compress_image(image, file_name, 8000000)
+        case 3: #Twitter
+            return compress_image(image, file_name, 5000000)
+            # ToDo: .GIF -> 15MB instead of 5MB
+        case 4: #LinkedIn
+            return compress_image(image, file_name, 5000000)
+        case 5: #Discord
+            return compress_image(image, file_name, 8000000)
+        case 6: #Messenger
+            return compress_image(image, file_name, 25000000)
         case _:
             return "Error"
 
@@ -31,9 +33,8 @@ def get_compressed_file_name(file_path):
     file_extension = complete_path[-1].split(".")[1]
 
     compressed_file = "pictures/" + file_name + "_compressed." + file_extension
-    compressed_optimized_file = "pictures/" + file_name + "_compressed_optimized." + file_extension
 
-    return [compressed_file, compressed_optimized_file]   
+    return compressed_file 
 
 '''
 This function prints the file information: size in pixels and size in MB
@@ -41,3 +42,26 @@ This function prints the file information: size in pixels and size in MB
 def print_file_info(file, file_path, display_text):
     print("Size of " + display_text + " picture: " + str(file.size[0]) + " X " + str(file.size[0]))
     print(display_text + " file size: " + str(size(os.path.getsize(file_path))) + " bytes")
+
+'''
+This function compresses an image until it reaches the desired final_size.
+If the image size is already bellow the final_size, it's not compressed.
+The compression is limited until a quality of 50% (successfull in most of the cases).
+'''
+def compress_image(image, file_path, final_size):
+    image.save(file_path,optimize=True,quality=100)
+    if len(Image.open(file_path).fp.read()) <= final_size:
+        return "No need to compress"
+    for i in range(95, 50, -5):
+        image.save(file_path,optimize=True,quality=i)
+        if len(Image.open(file_path).fp.read()) <= final_size:
+            return "Success"
+    return "Unsuccess"
+
+'''
+This function checks if the file extension is in the list of valid types.
+If it isn't, then prompts the user to select the new format or to cancel.
+'''
+def check_image_type(file_name, valid_types):
+    return "ToDo"    
+    
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 PILLOW (PIL) documentation: https://pillow.readthedocs.io/en/stable/
 '''
 
+from distutils.log import error
 from PIL import Image
 import tkinter as tk
 from tkinter import filedialog
@@ -15,13 +16,20 @@ file_path = filedialog.askopenfilename()
 foo = Image.open(file_path)
 print_file_info(foo, file_path, "Original")
 
-file_names = get_compressed_file_name(file_path)
+file_name = get_compressed_file_name(file_path)
 
-foo.save(file_names[0],quality=95)
-foo.save(file_names[1],optimize=True,quality=95)
+user_selection = input("Please select one platform:\n\n1.Facebook\n2.Instagram\n3.Twitter\n4.Linkedin\n5.Discord\n6.Messenger\n\nYour selection: ")
 
-compressed = Image.open(file_names[0])
-compressed_optimized = Image.open(file_names[1])
+result = get_platform(int(user_selection), foo, file_name)
 
-print_file_info(compressed, file_path, "Compressed")
-print_file_info(compressed_optimized, file_path, "Compressed and Optimized")
+match result:
+    case "Success":
+        print("Successfully compressed")
+        compressed = Image.open(file_name)
+        print_file_info(compressed, file_name, "Compressed")
+    case "Unsuccess":
+        error("Picture could not be compressed")
+    case "No need to compress":
+        print("Picture already with less than file size limit.")
+    case "Error":
+        error("Error")
