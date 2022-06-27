79c3e68f2e2490bd19d6c9e5890c43aa3bd4a988
Nuno Rodrigues de Castro Santos Silva <up201404676@fe.up.pt>
2022-06-27 16:04:20 +0100

    Added video compression functions

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-import os
+import os,ffmpeg
 from hurry.filesize import size
 from PIL import Image
 
@@ -112,3 +112,54 @@ def change_image_type(valid_types):
         return "Cancel"
     else:
         return valid_types[user_selection-1]
+
+import os,ffmpeg
+
+
+'''
+This function takes a video file and compresses it to the target_size defined (target_size is in kb)
+if target_size = 0, function determines ideal min size
+'''
+def compress_video(video_full_path, output_file_name, target_size):
+    # Reference: https://en.wikipedia.org/wiki/Bit_rate#Encoding_bit_rate
+    print("started")
+    min_audio_bitrate = 32000
+    max_audio_bitrate = 256000
+    
+    probe = ffmpeg.probe(video_full_path)
+    # Video duration, in s.
+    duration = float(probe['format']['duration'])
+    # Audio bitrate, in bps.
+    audio_bitrate = float(next((s for s in probe['streams'] if s['codec_type'] == 'audio'), None)['bit_rate'])
+    # Target total bitrate, in bps.
+    if target_size == 0:
+        target_size = get_best_min_size(video_full_path)
+    target_total_bitrate = (target_size * 1024 * 8) / (1.073741824 * duration)
+
+    # Target audio bitrate, in bps
+    if 10 * audio_bitrate > target_total_bitrate:
+        audio_bitrate = target_total_bitrate / 10
+        if audio_bitrate < min_audio_bitrate < target_total_bitrate:
+            audio_bitrate = min_audio_bitrate
+        elif audio_bitrate > max_audio_bitrate:
+            audio_bitrate = max_audio_bitrate
+    # Target video bitrate, in bps.
+    video_bitrate = target_total_bitrate - audio_bitrate
+
+    i = ffmpeg.input(video_full_path)
+    ffmpeg.output(i, os.devnull,**{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 1, 'f': 'mp4'}).overwrite_output().run()
+    ffmpeg.output(i, output_file_name,**{'c:v': 'libx264', 'b:v': video_bitrate, 'pass': 2, 'c:a': 'aac', 'b:a': audio_bitrate}).overwrite_output().run()
+    print("finished")
+
+
+
+'''
+This function returns the ideal min size of a video in kB
+'''
+def get_best_min_size(video_full_path):
+    probe = ffmpeg.probe(video_full_path)
+    # Video duration, in s.
+    duration = float(probe['format']['duration'])
+    ideal_size = (32000 + 100000) * (1.073741824 * duration) / (8 * 1024)
+    print("Ideal size (kb): " + str(ideal_size))
+    return ideal_size
